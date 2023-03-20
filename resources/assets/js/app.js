c56269beec14939bf1fc5a8f56ed2a074e16e199
Luis Majano <lmajano@gmail.com>
2023-03-20 18:17:29 +0100

    more vite standards to the default app templates

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,8 @@
-import "vite/dynamic-import-polyfill"
+import "vite/dynamic-import-polyfill";
 import Vue from "vue";
 import Hello from "./components/Hello.vue";
 import "../css/tailwind.css";
 
-let app = document.getElementById("app");
+let app = document.getElementById( "app" );
 
-new Vue({
-	components: {
-		Hello
-	}
-}).$mount(app);
+new Vue( { components: { Hello } } ).$mount( app );
